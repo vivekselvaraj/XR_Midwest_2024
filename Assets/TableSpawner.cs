df31124132914e7a81d7e7db77efc6b2780dee5e
Selvaraj <vive@UMICH.EDU>
2024-10-12 18:43:22 -0400

    add button to disable debug UI

--- Assets/TableSpawner.cs
+++ Assets/TableSpawner.cs
@@ -7,7 +7,16 @@ public class TableSpawner : MonoBehaviour
     public GameObject tablePrefab;
     public OVRSkeleton LeftSkeleton;
     public OVRSkeleton RightSkeleton;
-    
+    public GameObject DebugUI;
+
+    public void RemoveDebugUI() {
+        if (DebugUI.activeSelf) {
+            DebugUI.SetActive(false);
+        }
+        else {
+            DebugUI.SetActive(true);
+        }
+    }
     
     public void SpawnTable() {
         Transform leftThumbTipTransform = GetFingerTransformForSkeleton(LeftSkeleton, OVRSkeleton.BoneId.Hand_ThumbTip);
@@ -22,9 +31,24 @@ public class TableSpawner : MonoBehaviour
         // if a object with tag table exists, destroy it
         GameObject oldTable = GameObject.FindGameObjectWithTag("Table");
         if (oldTable != null) {
-            Destroy(oldTable);
+            // Destoy(oldTable);
+            Debug.Log("Table already exists");
+            return;
         }
         GameObject table = Instantiate(tablePrefab, midpoint, rotation);
+        
+        
+        // Passthrough enable for keyboard
+        GameObject scripts = GameObject.Find("Scripts");
+        OVRPassthroughLayer layer = scripts.GetComponent<OVRPassthroughLayer>();
+        GameObject passthroughSurface = table.transform.Find("Plane").gameObject;
+        layer.AddSurfaceGeometry(passthroughSurface, false);
+
+        // Disable the mesh renderer to avoid rendering the surface within Unity
+        MeshRenderer mr = passthroughSurface.GetComponent<MeshRenderer>();
+        if (mr) {
+            mr.enabled = false;
+        }
     }
 
     public Transform GetFingerTransformForSkeleton(OVRSkeleton skeleton, OVRSkeleton.BoneId boneId) {
